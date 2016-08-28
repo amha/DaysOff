7813529040535dfab541457e08a85a6bc2d31d0e
Amha Mogus <amha.mogus@gmail.com>
2016-08-28 13:07:36 -0400

    Implemented UI for MainActivity and CalendarDetailActivity.

--- app/src/main/java/amhamogus/com/daysoff/MainActivity.java
+++ app/src/main/java/amhamogus/com/daysoff/MainActivity.java
@@ -1,13 +1,31 @@
 package amhamogus.com.daysoff;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import amhamogus.com.daysoff.ui.CalendarDetailActivity;
+import amhamogus.com.daysoff.ui.CalendarItemFragment;
+import amhamogus.com.daysoff.ui.dummy.DummyContent;
+
+public class MainActivity extends AppCompatActivity implements CalendarItemFragment.OnListFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+        //TODO: Pass Calendar ID to CalendarDetailActivity
+//        if(item != null){
+//            Toast.makeText(MainActivity.this,  "Item number:" +item.id,
+//                    Toast.LENGTH_SHORT).show();
+//        }
+
+        Intent intent = new Intent(getApplicationContext(), CalendarDetailActivity.class);
+        startActivity(intent);
     }
 }
